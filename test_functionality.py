6c2618ae633f5cc23984c77827c307afb407621a
gdharra2 <46692795+gdharra2@users.noreply.github.com>
2019-08-20 12:23:41 -0500

    Updated test scripts for best practise

--- test_functionality.py
+++ test_functionality.py
@@ -1,13 +1,13 @@
-from utility import validate_input
+from utility import utility as u
 
 
 def test_valid_input():
-    assert validate_input('tests/images_with_directory.zip',3,2, True)
+    assert u.validate_input('tests/images_with_directory.zip',3,2, True)
 
 
 def test_invalid_input():
-    assert not validate_input('tests/images_without_directory.zip',4,2, True)
+    assert not u.validate_input('tests/images_without_directory.zip',4,2, True)
 
 
-def test_invalid_ifiles():
-    assert not validate_input('tests/test_invalidfiles.zip',2,1, True)
+def test_invalid_input_files():
+    assert not u.validate_input('tests/test_invalid_files.zip',2,1, True)
