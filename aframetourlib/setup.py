33ed06e0642b9cc5d9c7a6009706a0659fe83f95
Wallace <rwwalla2@illinois.edu>
2019-10-03 15:28:44 -0500

    aframetour 0.1.9

--- aframetourlib/setup.py
+++ aframetourlib/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name='aframetour',
   packages=['aframetour'],
-  version='0.1.8',
+  version='0.1.9',
   license='MIT',
   description='This library generates a web vr tour for a given zip file and grid size',
   author='Gaurav Dharra, Rob Wallace',
